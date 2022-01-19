7c1edaeb4911b373bb46e5a9c398498e3d75acb9
marinafischer <marinamirandafischer@gmail.com>
2022-01-19 16:18:50 -0300

    requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const request = await fetch(URL);
+  const response = await request.json();
+
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const URLQuery = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  const URLCategory = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const URLQueryCategory = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  let response = '';
+  if (!categoryId) response = await fetch(URLQuery);
+  if (!query) response = await fetch(URLCategory);
+  response = await fetch(URLQueryCategory);
+  return response.json();
 }
