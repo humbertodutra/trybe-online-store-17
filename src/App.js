cb62d5d8a391b2b0d8f2946d4bc05b17c982eedb
Dayana <dayanadiasgarcia@gmail.com>
2022-01-20 17:48:51 -0300

    Requisito 07

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import ProductDetails from './pages/ProductDetails';
 
 // Teste 1
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/:id" component={ ProductDetails } />
         <Route path="/cart" component={ Cart } />
         <Route path="/" component={ Home } />
       </Switch>
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductCard extends React.Component {
   render() {
-    const { thumbnail, price, title } = this.props;
+    const { thumbnail, price, title, id } = this.props;
     return (
-      <div data-testid="product" className="product-card">
-        <div className="img-div">
-          <img src={ thumbnail } alt={ title } />
-        </div>
-        <div className="info-div">
-          <h2>{ title }</h2>
-          <div className="price">
-            <span>{ price }</span>
+      <Link
+        data-testid="product-detail-link"
+        to={ `/${id}` }
+      >
+        <div
+          data-testid="product"
+          className="product-card"
+        >
+
+          <div className="img-div">
+            <img src={ thumbnail } alt={ title } />
+          </div>
+          <div className="info-div">
+            <h2>{ title }</h2>
+            <div className="price">
+              <span>{ price }</span>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     );
   }
 }
@@ -24,6 +34,7 @@ ProductCard.propTypes = {
   thumbnail: propTypes.string.isRequired,
   price: propTypes.number.isRequired,
   title: propTypes.string.isRequired,
+  id: propTypes.string.isRequired,
 };
 
 export default ProductCard;
