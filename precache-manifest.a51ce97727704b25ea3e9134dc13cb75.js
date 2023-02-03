f1525841df0341399f5346fc681701a27eadd114
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-03 19:02:59 -0300

    Updates

---
+++ precache-manifest.a51ce97727704b25ea3e9134dc13cb75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9768aedab4a4fb7f467604073ce614",
+    "url": "/trybe-online-store-17/index.html"
+  },
+  {
+    "revision": "8020c7b4b2f41c6370f7",
+    "url": "/trybe-online-store-17/static/css/main.aaac9626.chunk.css"
+  },
+  {
+    "revision": "42a90d68f0c67110e0ab",
+    "url": "/trybe-online-store-17/static/js/2.c87a54ad.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trybe-online-store-17/static/js/2.c87a54ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8020c7b4b2f41c6370f7",
+    "url": "/trybe-online-store-17/static/js/main.3b340ef1.chunk.js"
+  },
+  {
+    "revision": "d10ad8ad66abceb7c3be",
+    "url": "/trybe-online-store-17/static/js/runtime-main.4a454907.js"
+  }
+]);
