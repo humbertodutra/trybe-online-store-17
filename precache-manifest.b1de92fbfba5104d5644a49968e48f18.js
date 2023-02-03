6d72a9fb5c7332b31de67f85658507c550408ee4
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-03 16:58:44 -0300

    Updates

---
+++ precache-manifest.b1de92fbfba5104d5644a49968e48f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64739cc62dd6cc4450ea969c21e154fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbfa66215d9e6768926f",
+    "url": "/static/css/main.aaac9626.chunk.css"
+  },
+  {
+    "revision": "42a90d68f0c67110e0ab",
+    "url": "/static/js/2.c87a54ad.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c87a54ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfa66215d9e6768926f",
+    "url": "/static/js/main.268e6576.chunk.js"
+  },
+  {
+    "revision": "837674402e425cca2f8d",
+    "url": "/static/js/runtime-main.6839dbe2.js"
+  }
+]);
