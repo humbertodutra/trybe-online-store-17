4a2726aeb61b1a5039634852bf08dfba575b9491
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-03 17:03:42 -0300

    Updates

---
+++ precache-manifest.7a234d0f9431f14acbc1fa69a274d735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46ce77e7de088b52c1a24d2e344f4d7",
+    "url": "/humbertodutra/Trybe-online-store-17/index.html"
+  },
+  {
+    "revision": "584a26ecb47f007caeee",
+    "url": "/humbertodutra/Trybe-online-store-17/static/css/main.aaac9626.chunk.css"
+  },
+  {
+    "revision": "42a90d68f0c67110e0ab",
+    "url": "/humbertodutra/Trybe-online-store-17/static/js/2.c87a54ad.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/humbertodutra/Trybe-online-store-17/static/js/2.c87a54ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584a26ecb47f007caeee",
+    "url": "/humbertodutra/Trybe-online-store-17/static/js/main.c14084d6.chunk.js"
+  },
+  {
+    "revision": "ad545ffad84872578ac9",
+    "url": "/humbertodutra/Trybe-online-store-17/static/js/runtime-main.821a4e07.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1de92fbfba5104d5644a49968e48f18.js"
+  "/humbertodutra/Trybe-online-store-17/precache-manifest.7a234d0f9431f14acbc1fa69a274d735.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/humbertodutra/Trybe-online-store-17/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
